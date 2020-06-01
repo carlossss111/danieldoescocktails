b53bad2794f0dd2360273e3e5ef50dd18afbdd13
LinuxDaniel <danielrobinson1112@gmail.com>
2020-06-01 13:57:13 +0100

    Added touchscreen for tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -13,37 +13,41 @@ for (i = 0; i < contentList.length; i++){
     else{ contentList[i].style.display = "none";}
 }
 
+//Called whenever a tab is pressed.
+function tabHandler(event){
+    //Finds index of which tab was clicked.
+    var tab = event.target;
+    var tabIndex = tabArray.indexOf(tab);
+    //Uses that index to display the content with the matching index and to hide the content that does not match.
+    contentList.forEach(function(content){
+        if (contentList[tabIndex] === content){
+            content.style.display = "block";
+        }
+        else{
+            content.style.display = "none";
+        }
+    })
+
+    //If there is no custom colours then change the border below.
+    for (i = 0; i < tabArray.length; i++){
+        if(tab === tabArray[i]){
+            //If the tab has a default background colour, use that. If not change it seperated (for index.html).
+            if(!event.target.style.backgroundColor){
+                tab.style.borderBottom = "2px solid #c32222";
+            }
+            else{
+                tab.style.borderBottom = `2px solid ${tab.style.backgroundColor}`;
+            }
+        }
+        else{
+            tabArray[i].style.borderBottom = "";
+        }
+    }
+}
 
 //Event listener added for each tab.
 tabList.forEach( function(tab){
     //Onclick
-    tab.addEventListener('click',function(){
-        //Finds index of which tab was clicked.
-        var tabIndex = tabArray.indexOf(tab);
-        //Uses that index to display the content with the matching index and to hide the content that does not match.
-        contentList.forEach(function(content){
-            if (contentList[tabIndex] === content){
-                content.style.display = "block";
-            }
-            else{
-                content.style.display = "none";
-            }
-        })
-
-        //If there is no custom colours then change the border below.
-        for (i = 0; i < tabArray.length; i++){
-            if(tab === tabArray[i]){
-                //If the tab has a default background colour, use that. If not change it seperated (for index.html).
-                if(!tab.style.backgroundColor){
-                    tab.style.borderBottom = "2px solid #c32222";
-                }
-                else{
-                    tab.style.borderBottom = `2px solid ${tab.style.backgroundColor}`;
-                }
-            }
-            else{
-                tabArray[i].style.borderBottom = "";
-            }
-        }
-    })
+    tab.addEventListener('click',tabHandler);
+    tab.addEventListener('touchstart',tabHandler);
 })
