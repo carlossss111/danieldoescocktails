4b407602468a0264d17badf41303f3277c97fdf1
LinuxDaniel <danielrobinson1112@gmail.com>
2020-06-01 13:24:01 +0100

    universal tab.js script works on both index.html and resources.html

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -4,13 +4,22 @@ var tabList = document.querySelectorAll(".tab");
 var tabArray = [];//Needs to be in an array to use indexOf().
 for (i = 0; i < tabList.length; i++){ tabArray.push(tabList[i]);}
 var contentList = document.querySelectorAll(".tabcontent");
+//Initially only shows first content.
+for (i = 0; i < contentList.length; i++){
+    if (i === 0){ 
+        contentList[i].style.display = "block"; 
+        tabArray[i].style.borderBottom = "2px solid #c32222";
+    }
+    else{ contentList[i].style.display = "none";}
+}
+
 
 //Event listener added for each tab.
 tabList.forEach( function(tab){
     //Onclick
-    tab.addEventListener('click',function(event){
+    tab.addEventListener('click',function(){
         //Finds index of which tab was clicked.
-        var tabIndex = tabArray.indexOf(event.target);
+        var tabIndex = tabArray.indexOf(tab);
         //Uses that index to display the content with the matching index and to hide the content that does not match.
         contentList.forEach(function(content){
             if (contentList[tabIndex] === content){
@@ -20,5 +29,21 @@ tabList.forEach( function(tab){
                 content.style.display = "none";
             }
         })
+
+        //If there is no custom colours then change the border below.
+        for (i = 0; i < tabArray.length; i++){
+            if(tab === tabArray[i]){
+                //If the tab has a default background colour, use that. If not change it seperated (for index.html).
+                if(!tab.style.backgroundColor){
+                    tab.style.borderBottom = "2px solid #c32222";
+                }
+                else{
+                    tab.style.borderBottom = `2px solid ${tab.style.backgroundColor}`;
+                }
+            }
+            else{
+                tabArray[i].style.borderBottom = "";
+            }
+        }
     })
 })
