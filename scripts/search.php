c276bb0337e7165a6d12813274066ee5d4edcb9a
Daniel <danielrobinson1112@gmail.com>
2021-07-02 20:10:33 +0100

    Fixed indentation

--- scripts/search.php
+++ scripts/search.php
@@ -3,40 +3,41 @@
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
 
+//connect to mySQLi
 include "./keys.php";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+	die("Connection failed: " . mysqli_connect_error());
 }
 
+//query mySQLi
 $query = "SELECT * FROM $table;";
 $result = mysqli_query($conn,$query);
 
-$out = "";
+//print results
 if ($result->num_rows > 0) {
-  while($row = mysqli_fetch_assoc($result)){
-    $out .= "<td>";
-    $out .= " <img src=\"" . $row["image"] . "\">";
-    $out .= "</td>";
-    $out .= "<td class=\"itembox\">";
-    $out .= " <h2>" . $row["name"] . "</h2>";
-    $out .= " <ul class=\"ingredients\">";
-    $ingredientArr = explode("/",$row["ingredients"]);
-    foreach ($ingredientArr as $elem){
-      $out .= "   <li>$elem</li>";
-    }
-    $out .= " </ul>";
-    $out .= " <p class=\"description\">" . $row["description"];
-    $out .= " <span class=\"date\">" . $row["date"] . "</span></p>";
-    $out .= "</td>";
-  }
+	while($row = mysqli_fetch_assoc($result)){
+		echo "<td>";
+		echo "	<img src=\"" . $row["image"] . "\">";
+		echo "</td>";
+		echo "<td class=\"itembox\">";
+		echo "	<h2>" . $row["name"] . "</h2>";
+		echo " 	<ul class=\"ingredients\">";
+		$ingredientArr = explode("/",$row["ingredients"]);
+		foreach ($ingredientArr as $elem){
+			echo "	<li>$elem</li>";
+		}
+		echo "	</ul>";
+		echo "	<p class=\"description\">" . $row["description"];
+		echo "	<span class=\"date\">" . $row["date"] . "</span></p>";
+		echo "</td>";
+	}
 }
 else {
-  $out = "0 results";
+	echo "0 results";
 }
 
+//close mySQLi
 mysqli_close($conn);
-
-echo $out;
 ?> 
