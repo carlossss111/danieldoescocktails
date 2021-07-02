230e01ce233cb2621a2beebe3d93deda93117361
Daniel <danielrobinson1112@gmail.com>
2021-07-02 19:52:03 +0100

    Loops through all of SQL table on index.html

--- scripts/search.php
+++ scripts/search.php
@@ -10,7 +10,33 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
+$query = "SELECT * FROM $table;";
+$result = mysqli_query($conn,$query);
+
+$out = "";
+if ($result->num_rows > 0) {
+  while($row = mysqli_fetch_assoc($result)){
+    $out .= "<td>";
+    $out .= " <img src=\"" . $row["image"] . "\">";
+    $out .= "</td>";
+    $out .= "<td class=\"itembox\">";
+    $out .= " <h2>" . $row["name"] . "</h2>";
+    $out .= " <ul class=\"ingredients\">";
+    $ingredientArr = explode("/",$row["ingredients"]);
+    foreach ($ingredientArr as $elem){
+      $out .= "   <li>$elem</li>";
+    }
+    $out .= " </ul>";
+    $out .= " <p class=\"description\">" . $row["description"];
+    $out .= " <span class=\"date\">" . $row["date"] . "</span></p>";
+    $out .= "</td>";
+  }
+}
+else {
+  $out = "0 results";
+}
+
 mysqli_close($conn);
 
-echo "Connected successfully";
+echo $out;
 ?> 
