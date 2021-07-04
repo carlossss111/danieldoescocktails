9cb276e226b94fdff26f0afc77fb8814ad49fe90
Daniel <danielrobinson1112@gmail.com>
2021-07-04 10:58:47 +0100

    Now loads multiple cocktails (in wrong order)
    index.html table1 has been reordered with thead,tbody and tfoot
    so that the cocktails can be put in the right place easier.
    search.php has been edited to add trs and has also been rewritten
    for readability.

--- scripts/search.php
+++ scripts/search.php
@@ -18,24 +18,39 @@ $result = mysqli_query($conn,$query);
 //print results
 if ($result->num_rows > 0) {
 	while($row = mysqli_fetch_assoc($result)){
-		echo "<td>";
-		echo "	<img src=\"" . $row["image"] . "\">";
-		echo "</td>";
-		echo "<td class=\"itembox\">";
-		echo "	<h2>" . $row["name"] . "</h2>";
-		echo " 	<ul class=\"ingredients\">";
+		//split ingredients over '/' delimiter and put into string
+		$li = "";
 		$ingredientArr = explode("/",$row["ingredients"]);
 		foreach ($ingredientArr as $elem){
-			echo "	<li>$elem</li>";
+			$li .= "<li>$elem</li>";
 		}
-		echo "	</ul>";
-		echo "	<p class=\"description\">" . $row["description"];
-		echo "	<span class=\"date\">" . $row["date"] . "</span></p>";
-		echo "</td>";
+
+		//echo results row by row
+		$image = $row["image"];			$name = $row["name"];
+		$desc = $row["description"];	$date = $row["date"];
+		echo	"<tr>										";
+		echo	"	<td>									";
+		echo	"		<img src='$image'>					";
+		echo	"	</td>									";
+		echo	"	<td class='itembox'>					";
+		echo	"		<h2>$name</h2>						";
+		echo	"		<ul class='ingredients'>			";
+		echo	"			$li								";
+		echo	"		</ul>								";
+		echo	"		<p class='description'>				";
+		echo	" 			$desc							";
+		echo	"			<span class='date'>$date</span>	";
+		echo	"		</p>								";
+		echo	"	</td>									";
+		echo	"</tr>										";
 	}
 }
 else {
-	echo "0 results";
+	echo	"<tr>								";
+	echo	"	<td colspan='2'>				";
+	echo	"		<h2>No results found</h2>	";
+	echo	"	</td>							";
+	echo	"</tr>								";
 }
 
 //close mySQLi
