9df20763ccc0ee2d2cac04dd15829e06cf4db43e
Daniel <danielrobinson1112@gmail.com>
2021-07-07 14:50:48 +0100

    Refactored units.js to use functions

--- scripts/units.js
+++ scripts/units.js
@@ -1,74 +1,95 @@
-//The tickbox is labelled to be true if in metric. So Starts with that default.
-tickbox = document.getElementById("measurementTickbox");
-tickbox.checked = true;
+//array for storing the original metric units
+var storedUnits = [];
 
-//initialValue[] holds the initial ml measurements and is used when flipping back to metric.
-var initialValue = [];
-//Listener is called whenever the tickbox value changes.
-tickbox.addEventListener('change',function(){
+//given a nodelist of lis, convert them all to fluid ounces
+function toImperial(metricArr, trNum, ul){
+    //loop through every list item in the ul
+    for(let liNum = 0; liNum < ul.length; liNum++){
+        let li = ul[liNum];
 
-    //Every table tow is looped through (except the heading and footer).
-    var tr = document.querySelectorAll("#firstTable tr");
-    for (i = 1; i < (tr.length-1); i++) { 
-        //Each line of ingredients is stored in a nodeList.
-        let cocktailIngredients = tr[i].querySelector(".ingredients");
-        let li = cocktailIngredients.querySelectorAll("li");
+        //if there is no ml measurement, skip this list item
+        if(li.innerText.search("ml") == -1)
+            continue;
+        
+        //split str between 'ml' and return first half
+        let ml = li.innerText.split("ml").shift();
+        //store the original value
+        metricArr[trNum].push(ml);
 
-        //The metric array pushes another array inside itself, making it two dimensional.
-        initialValue.push([]);
-            
-        //If imperial...
-        if(tickbox.checked === false){
-            //For each line (in the given row):
-            li.forEach(function(line){
-                //If there is no 'ml' in a line, it will return -1, hence those results are discarded.
-                if(line.innerText.search("ml") !== -1){
-                    let milliliters = line.innerText.split("ml").shift();//Splits between 'ml', and returns the first half (using shift).
-
-                    //initialValue 2D array has the milliliter values stored for later.
-                    initialValue[i-1].push(milliliters);
-
-                    //ml converted to floz and rounded to a multiple of 0.25.
-                    let fluidOunces = Math.round((milliliters/29.574) /0.25) * 0.25;
-                    //Unicode changes the string to a fraction.
-                    let fluidOuncesStr = fluidOunces;
-                    if (fluidOunces === 0.25){ fluidOuncesStr = "\u00BC"}// 1/4
-                    else if (fluidOunces === 0.5){ fluidOuncesStr = "\u00BD"}// 1/2
-                    else if (fluidOunces === 0.75){ fluidOuncesStr = "\u00BE"}// /3/4
-                    else if (fluidOunces === 1.25){ fluidOuncesStr = "1\u00BC"}// 1 1/4
-                    else if (fluidOunces === 1.5){ fluidOuncesStr = "1\u00BD"}// 1 1/2
-                    else if (fluidOunces === 1.75){ fluidOuncesStr = "1\u00BE"}// 1 3/4
-
-                    //The text of the line is changes with the new measurements, and the original second half of the text.
-                    line.innerText = `${fluidOuncesStr}oz ${line.innerText.split("ml").pop()}`;
-                        
-                }
-            })
+        //convert to fluid ounces and fractional unicode if appropriate
+        let floz = Math.round((ml/29.574) /0.25) * 0.25;
+        let flozStr = floz;
+        switch(floz){
+            case 0.25:
+                flozStr = "\u00BC";// 1/4
+                break;
+            case 0.5:
+                flozStr = "\u00BD";// 1/2
+                break;
+            case 0.75:
+                flozStr = "\u00BE";// 3/4
+                break;
+            case 1.25:
+                flozStr = "1\u00BC";//1 1/4
+                break;
+            case 1.5:
+                flozStr = "1\u00BD";// 1 1/2
+                break;
+            case 1.75:
+                flozStr = "1\u00BE"//1 3/4
+                break;
         }
-        //If metric...
-        else{
-            //In each line, use initialValue[] to revert each line to how it was initially.
-            let n = 0;
-            li.forEach(function(line){
-                if(line.innerText.search("oz") !== -1){
-                    line.innerText = `${initialValue[i-1][n]}ml ${line.innerText.split("oz").pop()}`
-                    n++;
-                }
-            })
+        //replace the measurement and append the original second half of the str
+        li.innerText = `${flozStr}oz ${li.innerText.split("ml").pop()}`;
+    }
+    //return the array of original metric values for later use
+    return metricArr;
+}
+
+//given a nodelist of lis, convert them all back to their original values (metric)
+function toMetric(metricArr, trNum, ul){
+    let n = 0;
+    //if there is a measurement, rewrite it back to the original stored metric value
+    for(let liNum = 0; liNum < ul.length; liNum++){
+        let li = ul[liNum];
+        if(li.innerText.search("oz") !== -1){
+            li.innerText = `${metricArr[trNum][n]}ml ${li.innerText.split("oz").pop()}`
+            n++;
         }
-            
 
     }
-    //Once a cycle of metric-->imperial-->metric is complete clear the array (otherwise it will become too long.)
-    if(tickbox.checked){
-        initialValue = [];
+}
 
-        //For styling only.
+//handle styling and clear the storage after every metric->imperial->metric pattern
+function clearAndClean(metricArr, tickbox){
+    if(tickbox.checked == true){
         document.querySelector(".tickboxStyle").style.color = "#c32222";
+        return [];
     }
-
-    //For styling only.
-    if(tickbox.checked === false){
+    else{
         document.querySelector(".tickboxStyle").style.color = "transparent";
+        return metricArr;
     }
-})
+}
+
+//event handler for changing units
+function changeUnits(){
+    tickbox = document.getElementById("measurementTickbox");
+    var trList = document.querySelectorAll("#firstTable tbody tr");
+
+    //loop through every table row and call the functions the content
+    for (let trNum = 0; trNum < (trList.length); trNum++) { 
+        var ingredients = trList[trNum].querySelector(".ingredients");
+        var ul = ingredients.querySelectorAll("li");
+        storedUnits.push([]);//to 2D
+        
+        if(tickbox.checked == false)
+            storedUnits = toImperial(storedUnits, trNum, ul);
+        else
+            toMetric(storedUnits, trNum, ul);
+    }
+    storedUnits = clearAndClean(storedUnits, tickbox);
+}
+
+document.getElementById("measurementTickbox").checked = true;
+document.getElementById("measurementTickbox").addEventListener("change",changeUnits);
