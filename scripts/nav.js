d6832b8859f40fb6e1eed5c7e16ecca4add2c3dd
Daniel <danielrobinson1112@gmail.com>
2021-07-15 19:01:08 +0100

    Cleaned nav.js to look respectable.
    Functionised!

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,23 +1,54 @@
-//First word of the page title is stored.
-var pageTitle = document.title;
-var myLocation = pageTitle.split(" ")[0];
+/**
+ * nav.js - Daniel Robinson
+ * Navigation bar template.
+ *
+ */
 
-//index is in a different folder from the rest of the HTML, therefore needs slightly different links.
-var nav = document.createElement("nav");
-if (myLocation.toLowerCase() === "home"){
-    nav.innerHTML = "<ul><li id='navlogoContainer'><img src='./images/logo/Dan_Does_Cocktails_Light_Shadow.jpg' alt='danieldoescocktails' id='navlogo'><img src='./images/buttons/hamburgerButton.png' alt='button' id='hamburgerBtn' onclick='openNav()'/></li><li id='navfirst' class='slide'><a href='#'>Home</a></li><li class='slide'><a href='./pages/about.html'>About</a></li><li class='slide'><a href='./pages/cabinet.html'>Cabinet</a></li><li class='slide'><a href='./pages/resources.html'>Resources</a></li><li id='navlast' class='slide'><a href='./pages/blog.html'>Blog</a></li></ul>";
+//create nav template because links are the same for each page
+function createNav(){
+    var nav = document.createElement("nav");
+    nav.innerHTML = ""
+        +   "<ul>"
+        +       "<li id='navlogoContainer'>"
+        +           "<img src='/images/logo/Dan_Does_Cocktails_Light_Shadow.jpg' alt='danieldoescocktails' id='navlogo'>"
+        +           "<img src='/images/buttons/hamburgerButton.png' alt='button' id='hamburgerBtn' onclick='openNav()'/>"
+        +       "</li>"
+        +       "<li id='navfirst' class='slide'>"
+        +           "<a href='/index.html'>Home</a>"
+        +       "</li>"
+        +       "<li class='slide'>"
+        +           "<a href='/pages/about.html'>About</a>"
+        +       "</li>"
+        +       "<li class='slide'>"
+        +           "<a href='/pages/cabinet.html'>Cabinet</a>"
+        +       "</li>"
+        +       "<li class='slide'>"
+        +           "<a href='/pages/resources.html'>Resources</a>"
+        +       "</li>"
+        +       "<li id='navlast' class='slide'>"
+        +           "<a href='/pages/blog.html'>Blog</a>"
+        +       "</li>"
+        +   "</ul>";
+
+    document.body.appendChild(nav);
+    return nav;
 }
-else{
-    nav.innerHTML = "<ul><li id='navlogoContainer'><img src='../images/logo/Dan_Does_Cocktails_Light_Shadow.jpg' alt='danieldoescocktails' id='navlogo'><img src='../images/buttons/hamburgerButton.png' alt='button' id='hamburgerBtn' onclick='openNav()'/></li><li id='navfirst' class='slide'><a href='../index.html'>Home</a></li><li class='slide'><a href='./about.html'>About</a></li><li class='slide'><a href='./cabinet.html'>Cabinet</a></li><li class='slide'><a href='./resources.html'>Resources</a></li><li id='navlast' class='slide'><a href='./blog.html'>Blog</a></li></ul>";
+
+//embolden current page and remove unneeded hyperlink
+function highlightCurrentPage(nav){
+    var currentPage = window.location.pathname.split("/").pop();
+    var pageList = nav.querySelectorAll("a");
+
+    pageList.forEach(function(givenPage){
+        var href = givenPage.href;
+        href = href.split("/").pop();
+        if(href == currentPage){
+            givenPage.innerHTML = `<strong>${givenPage.innerHTML}</strong>`
+            givenPage.href = "#";
+        }
+    })
 }
 
-//Each link to another page is checked against the page title and put in bold if there is a match.
-pageList = nav.querySelectorAll("a")
-pageList.forEach(function(page){
-    if (page.innerHTML.toLowerCase() === myLocation.toLowerCase()){
-        page.innerHTML = `<strong>${page.innerHTML}</strong>`;
-    }
-})
-
-//HTML for the navbar is output as a child of the body.
-document.body.appendChild(nav);
+//MAIN
+var nav = createNav();
+highlightCurrentPage(nav);
