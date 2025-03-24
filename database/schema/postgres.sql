b8ec6a762d48674b15b1dd310c1e775f281b4c7f
Daniel <danielrobinson1112@gmail.com>
2025-03-24 21:22:59 +0000

    Create docker-compose and add postgres db

---
+++ database/schema/postgres.sql
@@ -0,0 +1,26 @@
+
+-- DanielDoesCocktails DB Version 2.0
+
+-- SCHEMA 
+
+CREATE TABLE db_version (
+    version VARCHAR(16),
+    create_time TIMESTAMP,
+    last_update_time TIMESTAMP
+);
+
+CREATE TABLE cocktails (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    image_path VARCHAR(256) NOT NULL,
+    ingredients VARCHAR(1024) NOT NULL,
+    description VARCHAR(1024) NOT NULL,
+    date TIMESTAMP NOT NULL
+);
+
+
+
+-- DEFAULT VALUES
+
+INSERT INTO db_version (version, create_time, last_update_time) VALUES ('2.0', now()::timestamp, now()::timestamp);
+
