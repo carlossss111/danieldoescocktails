2a860ff779d3730babfad5990cccbb1bd664e79a
Daniel <danielrobinson1112@gmail.com>
2025-03-30 13:00:21 +0100

    Add healthcheck to backend

---
+++ backend/src/healthcheck.py
@@ -0,0 +1,16 @@
+import requests
+
+
+URL = "http://localhost:5000/ping"
+TIMEOUT = 10 # seconds
+
+
+if __name__ == "__main__":
+    response = requests.get(URL, timeout=TIMEOUT)
+    if not response.ok:
+        exit(1)
+    if not response.json().get("status") == "OK":
+        exit(1)
+
+    exit(0)
+
