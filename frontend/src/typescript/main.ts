c7e060e8cd7994bb3a3bf3b40fbd262d12eb9d62
Daniel <danielrobinson1112@gmail.com>
2025-05-22 15:53:12 +0100

    Add recommendations page and more css

--- frontend/src/typescript/main.ts
+++ frontend/src/typescript/main.ts
@@ -16,20 +16,43 @@ const NAV_CONTENT_CLASS = ".slide";
 
 
 function main() {
-    const tabManager: TabManager
-                = new TabManager(TAB_CLASS, TAB_CONTENT_CLASS);
-    const unitsManager: UnitsManager 
-                = new UnitsManager(COCKTAIL_UNIT_TICKBOX_ID);
-    const cocktailTableManager: TableRowGetter 
-                = new CocktailGetter(COCKTAIL_BACKEND_ENDPOINT, BACKEND_PORT);
-    const travelTableManager: TableRowGetter
-                = new TravelGetter(TRAVEL_BACKEND_ENDPOINT, BACKEND_PORT);
-    const cocktailTableListener: TableActionListener 
-                = new TableActionListener(cocktailTableManager, unitsManager, COCKTAIL_TABLE_ANCHOR_ID, COCKTAIL_TABLE_SEARCH_ID, COCKTAIL_TABLE_MORE_ID);
-    const travelTableListener: TableActionListener 
-                = new TableActionListener(travelTableManager, unitsManager, TRAVEL_TABLE_ANCHOR_ID, COCKTAIL_TABLE_SEARCH_ID, TRAVEL_TABLE_MORE_ID);
-    const navManager: Navbar
-                = new Navbar(NAV_BUTTON_ID, NAV_CONTENT_CLASS);
+    try {
+        const tabManager: TabManager
+                    = new TabManager(TAB_CLASS, TAB_CONTENT_CLASS);
+    }
+    catch (err) {
+        if (!(err instanceof ReferenceError))
+            new Error(err)
+        console.log("(tabs.js not included in HTML)");
+    }
+
+    try {
+        const cocktailTableManager: TableRowGetter 
+                    = new CocktailGetter(COCKTAIL_BACKEND_ENDPOINT, BACKEND_PORT);
+        const travelTableManager: TableRowGetter
+                    = new TravelGetter(TRAVEL_BACKEND_ENDPOINT, BACKEND_PORT);
+        const unitsManager: UnitsManager 
+                    = new UnitsManager(COCKTAIL_UNIT_TICKBOX_ID);
+        const cocktailTableListener: TableActionListener 
+                    = new TableActionListener(cocktailTableManager, unitsManager, COCKTAIL_TABLE_ANCHOR_ID, COCKTAIL_TABLE_SEARCH_ID, COCKTAIL_TABLE_MORE_ID);
+        const travelTableListener: TableActionListener 
+                    = new TableActionListener(travelTableManager, unitsManager, TRAVEL_TABLE_ANCHOR_ID, COCKTAIL_TABLE_SEARCH_ID, TRAVEL_TABLE_MORE_ID);
+    }
+    catch (err) {
+        if (!(err instanceof ReferenceError))
+            new Error(err)
+        console.log("(units.js or cocktail_table.js not included in HTML)")
+    }
+
+    try { 
+        const navManager: Navbar
+                    = new Navbar(NAV_BUTTON_ID, NAV_CONTENT_CLASS);
+    }
+    catch (err) {
+        if (!(err instanceof ReferenceError))
+            new Error(err)
+        console.warn("nav.js not included in HTML.")
+    }
 }
 
 main();
